15b7560e4933af1caa59970fb7459a98f895b834
sahil kashetwar <kashetwartechlab@gmail.com>
2020-10-27 09:49:35 +0530

    first build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ConfigurationDialog from './dialog/dialog';
+import Sample from './sample/sample';
+import CKEditor from '@ckeditor/ckeditor5-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// NOTE: Use the editor from source (not a build)!
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+const editorConfiguration = {
+	plugins: [Essentials, Bold, Italic, Paragraph],
+	toolbar: ['bold', 'italic'],
+};
+
+export default class App extends Component {
+	state = {
+		configuration: null,
+	};
+	render() {
+		console.log(this.state.configuration);
+		if (!this.state.configuration) {
+			return (
+				<ConfigurationDialog
+					onSubmit={(configuration) => this.setState({ configuration })}
+				/>
+			);
+		}
+
+		return <Sample configuration={this.state.configuration} />;
+	}
 }
-
-export default App;
